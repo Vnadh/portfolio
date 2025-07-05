07784cd888944e683bdd8f8274e80e1ce28b450f
Vnadh <106485321+Vnadh@users.noreply.github.com>
2025-07-05 18:56:43 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // IMPORTANT for GitHub Pages
+  },
+  // Replace 'your-repository-name' with your actual GitHub repository name
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
 };
 
 export default nextConfig;
